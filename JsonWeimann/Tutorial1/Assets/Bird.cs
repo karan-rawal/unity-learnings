febefdc6e13c6c0d6dbbe78730238c2ab712e299
--global <kr14nov@outlook.com>
2020-08-12 23:03:40 +0530

    Implemented bird launch and reloading game.

---
+++ JsonWeimann/Tutorial1/Assets/Bird.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] private float launchPower = 350;
+
+    private Vector3? _initialPosition = null;
+    private bool _birdWasLaunched;
+    private float _timeSittingAround;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 10 ||
+            transform.position.y < -10 ||
+            transform.position.x > 10 ||
+            transform.position.x < -10 ||
+            _timeSittingAround > 3)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        if (_initialPosition != null)
+        {
+            Vector3 directionToInitialPosition = (Vector3)(_initialPosition - transform.position);
+            GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * launchPower);
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+            _birdWasLaunched = true;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position =  new Vector3(newPosition.x, newPosition.y);
+    }
+}
